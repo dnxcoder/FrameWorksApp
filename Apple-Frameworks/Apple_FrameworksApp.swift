e027185092ac3edc78a2ee87db6ba2e7fc9b8a02
Denis Coder <dnxcoder@gmail.com>
2024-01-20 09:28:02 -0600

    first commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Denis Coder on 1/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            FrameworkGridView()
+        }
+    }
+}
